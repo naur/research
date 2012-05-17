382f31343a90b1f635e6bd929e60f453f760c0f3
naur <naur@live.cn>
2012-05-17 12:06:02 +0800

    work-ver-2

---
+++ naure-home/src/main/java/org/naure/home/web/controllers/EngController.java
@@ -0,0 +1,11 @@
+package org.naure.home.web.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jiaruizhi
+ * Date: 5/17/12
+ * Time: 11:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EngController {
+}
