91980564d54b606c08f2dc05c0065c2457edc463
naur <naur@live.cn>
2012-06-20 13:23:23 -0700

    work-ver-1

--- naure-web/src/main/webapp/js/naure.overlay.js
+++ naure-web/src/main/webapp/js/naure.overlay.js
@@ -57,8 +57,8 @@
                 '   <section></section>' +
                 '   <section class="buttons">' +
                 '       <input type="button" title="Clean" onclick="NAURE.Message.empty();"/>' +
-                '       <input style="display: none;" title="Reset Graph" type="button">' +
-                '       <a class="help_button" title="Help Page" href="about/" target="_blank">&nbsp;</a>' +
+                '       <input style="display: none;" title="Reset Graph" type="button" />' +
+                '       <a class="help_button" title="Help Page" href="about/" target="_blank" />&nbsp;</a>' +
                 '       <input type="button" class="minimize" title="Minimize" onclick="NAURE.Message.empty();"/>' +
                 '   </section>' +
                 '</section>';
