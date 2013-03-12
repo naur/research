405f298acdec71937c19f3185ef232d2a59c986d
naur <naur@live.cn>
2013-03-12 18:55:43 +0800

    work-load.js

---
+++ naure-integrate/src/main/webapp/js/load.js
@@ -0,0 +1,27 @@
+var load = {
+    css: {dom: "",
+        hrefs: [
+            "/Research/projects/research/naure-integrate/src/main/webapp/css/global.css",
+            "/Research/projects/research/naure-integrate/src/main/webapp/css/modern.css"
+        ]
+    },
+    script: {
+        srcs: [
+            "/Research/projects/research/naure-integrate/src/main/webapp/js/require.js",
+            "/Research/projects/research/naure-integrate/src/main/webapp/js/require.config.js"
+        ]
+    }
+};
+
+var scripts = document.getElementsByTagName("script");
+var params = scripts[scripts.length - 1].getAttribute('data');
+
+if ("css" == params)
+    for (var index in load.css.hrefs) {
+        document.write("<link href=" + load.css.hrefs[index] + " rel=\"stylesheet\" type=\"text/css\"/>");
+    }
+
+if ("script" == params)
+    for (var index in load.script.srcs) {
+        document.write("<script type=\"text/javascript\" src=\"" + load.script.srcs[index] + "\"></script>");
+    }
