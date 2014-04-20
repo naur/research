99271bd2c2e518352e55a337d4be18e52be867f5
naur <naur@live.cn>
2014-04-20 18:51:27 +0800

    home

--- naure-integrate/src/main/webapp/js/core/naure.http.js
+++ naure-integrate/src/main/webapp/js/core/naure.http.js
@@ -6,8 +6,8 @@
  * Description：
  *          提供 http 工具集。
  *  有两个版本
- *          【ssa.http.ajax.js】：简单的封装 $.ajax
- *         【ssa.http.xsl.js】:   封装 $.ajax  和 ajaxslt ， 可以进行 获取xml 和 xsl 的格式化
+ *          【naure.http.ajax.js】：简单的封装 $.ajax
+ *         【naure.http.xsl.js】:   封装 $.ajax  和 ajaxslt ， 可以进行 获取xml 和 xsl 的格式化
  *
  */
 
