888def123bbe58ae768dc3142747eb5e8d07301a
naur <naur@live.cn>
2014-04-22 13:27:33 +0800

    work

---
+++ naure-research/src/main/java/org/naure/research/web/controllers/LabsController.java
@@ -0,0 +1,19 @@
+/*
+ * @(#) LabsController.java 2014-04-22
+ *
+ * Copy Right@ 纽海信息技术有限公司
+ */
+package org.naure.research.web.controllers;
+
+/**
+ * <pre>
+ * author jiaruizhi
+ *
+ * 创建日期: 2014-04-22 13:11
+ * 修改人 :
+ * 修改说明:
+ * 评审人 ：
+ * </pre>
+ */
+public class LabsController {
+}
