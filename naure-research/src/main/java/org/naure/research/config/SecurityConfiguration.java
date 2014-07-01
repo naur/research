a069eef50b3c4729e1ba79f12ad40cce68eccc35
U-NAUR\Administrator <Administrator@NAUR.(none)>
2014-07-01 23:37:30 +0800

    home

---
+++ naure-research/src/main/java/org/naure/research/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+/*
+ * @(#) SecurityConfig.java 2014-59-01
+ *
+ * Copy Right@ NAURE.ORG
+ */
+
+package org.naure.research.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <pre>
+ * author Administrator
+ *
+ * 创建日期: 2014-59-01
+ * 修改人 :
+ * 修改说明:
+ * 评审人 ：
+ * </pre>
+ */
+@Configuration
+public class SecurityConfiguration {
+    public @Value("${stock.realtime.uri}") String stockRealtimeUri;
+    public @Value("${stock.capital.uri}")  String stockCapitalUri;
+    public @Value("${stock.history.uri}")  String stockHistoryUri;
+}
--- naure-research/src/main/java/org/naure/research/web/services/StockService.java
+++ naure-research/src/main/java/org/naure/research/web/services/StockService.java
@@ -5,10 +5,30 @@
  */
 package org.naure.research.web.services;
 
+import com.sun.javafx.binding.StringFormatter;
+import org.apache.xerces.parsers.XMLDocumentParser;
+import org.apache.xerces.parsers.XMLParser;
+import org.exolab.castor.xml.util.XMLParserUtils;
+import org.naure.common.util.RequestClient;
+import org.naure.research.config.SecurityConfiguration;
+import org.naure.repositories.models.finance.Stock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.castor.CastorMarshaller;
+import org.springframework.stereotype.Service;
+
+import javax.xml.transform.stream.StreamSource;
+import java.beans.XMLDecoder;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringBufferInputStream;
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.List;
+
 /**
  * <pre>
  * author jiaruizhi
- *
  *
  * 创建日期: 2014-12-01
  * 修改人 :
@@ -16,5 +36,60 @@ package org.naure.research.web.services;
  * 评审人 ：
  * </pre>
  */
+@Service
 public class StockService {
+    //股票配置信息
+    @Autowired
+    private SecurityConfiguration securityConfiguration;
+    @Autowired
+    private CastorMarshaller castorMarshaller;
+    //请求的类型
+    private String type = "xml";
+
+    /**
+     * 获取股票历史数据
+     *
+     * @param stock     [sz000010]
+     * @param beginDate [19950802]
+     * @param endDate   [20120723]
+     */
+    public Stock getHistory(String stock, Date beginDate, Date endDate) throws IOException {
+        String result = RequestClient.getInstance().get(
+                MessageFormat.format(securityConfiguration.stockHistoryUri, stock, beginDate, endDate, type)
+        );
+        StreamSource stream = new StreamSource(result);
+        castorMarshaller.unmarshal(stream);
+
+        return null;
+    }
+
+    /**
+     * 获取股票股本相关信息
+     *
+     * @param stock [sz300197]
+     */
+    public Stock getCapital(String stock) throws IOException {
+        String result = RequestClient.getInstance().get(
+                MessageFormat.format(securityConfiguration.stockCapitalUri, stock)
+        );
+        StreamSource stream = new StreamSource(result);
+        castorMarshaller.unmarshal(stream);
+
+        return null;
+    }
+
+    /**
+     * 获取股票实时信息
+     *
+     * @param stock [shxxxxxx,szxxxxxx]
+     */
+    public List<Stock> getRealtime(String... stock) throws IOException {
+        String result = RequestClient.getInstance().get(
+                MessageFormat.format(securityConfiguration.stockCapitalUri, stock.toString())
+        );
+        StreamSource stream = new StreamSource(result);
+        castorMarshaller.unmarshal(stream);
+
+        return null;
+    }
 }
