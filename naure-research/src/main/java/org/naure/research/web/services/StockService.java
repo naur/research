f3db6f666ad935087469dd5d3dd656d3f1699419
naur <naur@live.cn>
2014-07-01 18:12:34 +0800

    work

---
+++ naure-research/src/main/java/org/naure/research/web/services/StockService.java
@@ -0,0 +1,20 @@
+/*
+ * @(#) StockService.java 2014-12-01
+ * 
+ * Copy Right@ 纽海信息技术有限公司
+ */
+package org.naure.research.web.services;
+
+/**
+ * <pre>
+ * author jiaruizhi
+ *
+ *
+ * 创建日期: 2014-12-01
+ * 修改人 :
+ * 修改说明:
+ * 评审人 ：
+ * </pre>
+ */
+public class StockService {
+}
