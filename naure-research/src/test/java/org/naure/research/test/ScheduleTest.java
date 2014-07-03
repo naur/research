261f27feb2832955e115ee806172fe339fc80bed
naur <naur@live.cn>
2014-07-03 23:14:03 +0800

    home

---
+++ naure-research/src/test/java/org/naure/research/test/ScheduleTest.java
@@ -0,0 +1,62 @@
+/*
+ * @(#) ScheduleTest.java 2014-54-03
+ *
+ * Copy Right@ NAURE.ORG
+ */
+
+package org.naure.research.test;
+
+import it.sauronsoftware.cron4j.Scheduler;
+import org.junit.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * <pre>
+ * author Administrator
+ *
+ * 创建日期: 2014-54-03
+ * 修改人 :
+ * 修改说明:
+ * 评审人 ：
+ * </pre>
+ */
+public class ScheduleTest {
+
+    @Test
+    public void cron4jTest() {
+        final AtomicInteger atomicInteger = new AtomicInteger();
+        atomicInteger.set(0);
+        Scheduler scheduler = new Scheduler();
+        scheduler.schedule("/5 * * * *", new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("count: " + String.valueOf(atomicInteger.addAndGet(1)));
+            }
+        });
+        scheduler.start();
+        try {
+            Thread.sleep(1L * 60L * 1000L);
+        } catch (InterruptedException e) {
+        }
+        scheduler.stop();
+    }
+
+    @Test
+    public void scheduledTest() {
+        final AtomicInteger atomicInteger = new AtomicInteger();
+        atomicInteger.set(0);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        Runnable work = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("count: " + atomicInteger.get());
+            }
+        };
+        //ScheduledFuture scheduledFuture = scheduler.schedule()
+        //scheduler.set
+    }
+}
