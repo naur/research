c8e740daac76a7cb911825ca24aa80e3b57855ae
naur <naur@live.cn>
2014-08-01 15:00:06 +0800

    work

--- config/env.groovy
+++ config/env.groovy
@@ -1,30 +1,26 @@
+naure {
+    log {
+        file {
+            path: '/var/web/logs'
+        }
+    }
+}
+
+mongodb {
+    host = '127.0.0.1'
+    port = '27017'
+    dbname = 'naure'
+    username = 'admin'
+    password = 'admin'
+}
+
+berkeley {
+    file = '/var/db/naure'
+}
+
 environments {
-    naure {
-        log {
-            file {
-                path: '/var/web/logs'
-            }
-        }
-    }
-    development {
-        mongodb {
-            host = '127.0.0.1'
-            port = '27017'
-            dbname = 'naure'
-            username = 'admin'
-            password = 'admin'
-        }
-
-        berkeley {
-            file = '/var/db/naure'
-        }
-    }
-
     production {
         mongodb {
-            host = '127.0.0.1'
-            port = '27017'
-            dbname = 'naure'
             username = 'dbuser'
             password = '19830618'
         }
