9b1d3372cadc6f919c0e79c299672d5a1cd1175a
naur <naur@live.cn>
2014-08-08 16:27:41 +0800

    home

--- integrate/naure-research/src/main/webapp/js/research/house.js
+++ integrate/naure-research/src/main/webapp/js/research/house.js
@@ -8,13 +8,18 @@ var global = {
     geocoder: null,
     //经度：114.38484,  维度：30.478435
     //北京：39.904214, 116.40741300000002);
-    center: {coordinate: [30.47856, 114.40844], name: ''},
+    center: {coordinate: [114.40844, 30.47856], name: ''},
     points: [
-        {coordinate: [30.47856, 114.40844], name: '工作地方', addr: '光谷软件园', city: '武汉市'},
-        {coordinate: [30.40364, 114.41549], name: '保利清能西海岸', addr: '', city: '武汉市'},
-        {coordinate: [30.43973, 114.43742], name: '平安光谷春天', addr: '', city: '武汉市'},
-        {coordinate: [30.48021, 114.51558], name: '朗诗里程', addr: '', city: '武汉市'},
-        {coordinate: [30.49713, 114.53742], name: '光谷北大资源.山水年华', addr: '', city: '武汉市'}
+//        {point: [30.47856, 114.40844], name: '工作地方', addr: '武汉市东湖高新技术开发区关山大道1号 光谷软件园F6栋6楼'},
+//        {point: [30.40364, 114.41549], name: '保利清能西海岸', addr: ''},
+//        {point: [30.43973, 114.43742], name: '平安光谷春天', addr: ''},
+//        {point: [30.48021, 114.51558], name: '朗诗里程', addr: ''},
+//        {point: [30.49713, 114.53742], name: '光谷北大资源.山水年华', addr: ''}
+        {coordinate: [114.412589, 30.485188], name: '工作地方', addr: '武汉市东湖高新技术开发区关山大道1号 光谷软件园F6栋6楼', city: '武汉市'},
+        {coordinate: [114.421697, 30.409536], name: '保利清能西海岸', addr: '江夏杨桥湖大道与栗庙路交汇处', city: '武汉市'},
+        {coordinate: [114.44384, 30.446165], name: '平安光谷春天', addr: '东湖高新开发区 高新六路15号', city: '武汉市'},
+        {coordinate: [114.521633, 30.488825], name: '朗诗里程', addr: '东湖高新 高新大道光谷六路与神墩二路交汇处', city: '武汉市'},
+        {coordinate: [114.544468, 30.502159], name: '光谷北大资源.山水年华', addr: '武汉市东湖高新区珊瑚北路与九峰二路交汇处', city: '武汉市'}
     ],
     maps: {
         google: {
@@ -43,16 +48,28 @@ var global = {
                 marker.bindTo('position', mapLabel);
             },
             parsePoint: function (point) {
-                return new google.maps.LatLng(point.coordinate[0], point.coordinate[1]);
+                return new google.maps.LatLng(point.coordinate[1], point.coordinate[0]);
             }
         },
-        baidu: {
+        baidu: {    //http://api.map.baidu.com/lbsapi/getpoint/index.html
             zoom: 15,
+            create: function (container, center, zoom) {
+                var map = new BMap.Map(container);
+                map.centerAndZoom(center, zoom);
+                map.addControl(new BMap.NavigationControl());   //地图平移缩放控件，PC端默认位于地图左上方，它包含控制地图的平移和缩放的功能。移动端提供缩放控件，默认位于地图右下方。
+                map.addControl(new BMap.ScaleControl());        //比例尺控件，默认位于地图左下方，显示地图的比例关系。
+                map.addControl(new BMap.OverviewMapControl());  //缩略地图控件，默认位于地图右下方，是一个可折叠的缩略地图。
+                //map.addControl(new BMap.GeolocationControl());  //定位控件，针对移动端开发，默认位于地图左下方。
+                map.addControl(new BMap.MapTypeControl());      //地图类型控件，默认位于地图右上方。
+                map.setCurrentCity("武汉"); // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
+                map.enableScrollWheelZoom();                            //启用滚轮放大缩小
+                return map;
+            },
             getCoordinate: function (point) {
                 // 创建地址解析器实例
                 var myGeo = new BMap.Geocoder();
                 // 将地址解析结果显示在地图上，并调整地图视野
-                myGeo.getPoint(point.attr, function (coord) {
+                myGeo.getPoint(point.addr, function (coord) {
                     if (coord) {
                         console.log(JSON.stringify(coord));
                         point.coordinate = coord;
@@ -71,19 +88,7 @@ var global = {
             },
             parsePoint: function (point) {
                 //Point(lng 经度:Number, lat 纬度:Number)
-                return new BMap.Point(point.coordinate[1], point.coordinate[0]);
-            },
-            create: function (container, center, zoom) {
-                var map = new BMap.Map(container);
-                map.centerAndZoom(center, zoom);
-                map.addControl(new BMap.NavigationControl());   //地图平移缩放控件，PC端默认位于地图左上方，它包含控制地图的平移和缩放的功能。移动端提供缩放控件，默认位于地图右下方。
-                map.addControl(new BMap.ScaleControl());        //比例尺控件，默认位于地图左下方，显示地图的比例关系。
-                map.addControl(new BMap.OverviewMapControl());  //缩略地图控件，默认位于地图右下方，是一个可折叠的缩略地图。
-                //map.addControl(new BMap.GeolocationControl());  //定位控件，针对移动端开发，默认位于地图左下方。
-                map.addControl(new BMap.MapTypeControl());      //地图类型控件，默认位于地图右上方。
-                map.setCurrentCity("武汉"); // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
-                map.enableScrollWheelZoom();                            //启用滚轮放大缩小
-                return map;
+                return new BMap.Point(point.coordinate[0], point.coordinate[1]);
             },
             marker: function (map, point) {
                 map.addOverlay(new BMap.Marker(point.coordinate));
@@ -104,6 +109,15 @@ var global = {
                     {strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5}
                 );
                 map.addOverlay(polyline);
+            },
+            navigation: function (map, src, dst) {
+                var driving = new BMap.TransitRoute(map, {
+                    renderOptions: {
+                        map: map,
+                        autoViewport: true
+                    }
+                });
+                driving.search(src.coordinate, dst.coordinate);
             }
         }
     }
@@ -111,15 +125,18 @@ var global = {
 
 function init(map) {
     for (var index in global.points) {
-        global.points[index].point = map.parsePoint(global.points[index]);
+        global.points[index].coordinate = map.parsePoint(global.points[index]);
     }
     global.center = map.parsePoint(global.center);
 
     global.map = map.create('map', global.center, map.zoom);
-    for (var index in global.points) {
-        map.marker(global.map, global.points[index]);
+    for (var i = 0; i < global.points.length; i++) {
+        map.marker(global.map, global.points[i]);
+        //map.line(map, global.points);
+        if (0 != i) {
+            map.navigation(global.map, global.points[0], global.points[i]);
+        }
     }
-    //map.line(map, global.points);
 }
 
 //require(['loading', 'research-template', 'google.maps', 'google.maplabel'], function (NAURE) {
