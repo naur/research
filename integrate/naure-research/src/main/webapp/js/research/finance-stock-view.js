c42b5f29bbcf2eba72e5a9fa5e26ce4db498fe9c
naur <naur@live.cn>
2014-09-14 23:20:48 +0800

    home

--- integrate/naure-research/src/main/webapp/js/research/finance-stock-view.js
+++ integrate/naure-research/src/main/webapp/js/research/finance-stock-view.js
@@ -12,12 +12,22 @@
 /*-------------------- 全局变量 START ----------------*/
 
 var global = {
-    queryUri: '/finance/stock' // /{type}/{code}/{start}/{end}
+    queryUri: '/finance/stock', // /{type}/{code}/{start}/{end}
+    dom: {
+        start: '#start',
+        end: '#end'
+    }
 };
 
 /*-------------------- 全局变量 END ------------------*/
 
 /*-------------------- 函数 START --------------------*/
+
+function init() {
+    $(global.dom.start).val(new Date());
+    $(global.dom.end).val(new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000));
+}
+
 /*-------------------- 函数 END ----------------------*/
 
 /*-------------------- 事件 START --------------------*/
