e428d89c47daccd7c3fa7e9a07ff9010871df42e
naur <naur@live.cn>
2014-12-30 09:35:33 +0800

    home

---
+++ core/naure-common/src/main/java/org/naure/common/GlobalConstants.java
@@ -0,0 +1,21 @@
+/*
+ * @(#) GlobalConstants.java 2014-12-30
+ * 
+ * Copy Right@ 纽海信息技术有限公司
+ */
+package org.naure.common;
+
+/**
+ * <pre>
+ * author jiaruizhi
+ *
+ *
+ * 创建日期: 2014-29-30
+ * 修改人 :
+ * 修改说明:
+ * 评审人 ：
+ * </pre>
+ */
+public interface GlobalConstants {
+    public static final String ZERO_PERCENTAGE = "0%";
+}
